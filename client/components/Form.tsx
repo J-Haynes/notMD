3846164ae30a1b60648e87890a497d196ae6d09f
sam-middleton <16142988+SamMiddleton@users.noreply.github.com>
2023-03-24 11:02:06 +1300

    form created

--- client/components/Form.tsx
+++ client/components/Form.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react'
+
 export interface FormData {
   primary: string
   secondary: string
@@ -9,11 +11,31 @@ interface Props {
 }
 
 function Form({ onSubmit }: Props) {
+  const [formData, setFormData] = useState({
+    primary: '',
+    secondary: '',
+    tertiary: '',
+  })
+
   return (
-    <form onSubmit={}>
+    <form onSubmit={() => onSubmit(formData)}>
       <label htmlFor="primary">Primary Symptom: </label>
+      <input type="text" name="primary" value={formData.primary} id="primary" />
       <label htmlFor="secondary">Secondary Symptom: </label>
+      <input
+        type="text"
+        name="secondary"
+        value={formData.secondary}
+        id="secondary"
+      />
       <label htmlFor="tertiary">Tertiary Symptom: </label>
+      <input
+        type="text"
+        name="tertiary"
+        value={formData.tertiary}
+        id="tertiary"
+      />
+      <button type="submit">Submit</button>
     </form>
   )
 }
