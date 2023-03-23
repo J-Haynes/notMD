c5ccaf3dfc1cd8d25f986cd7eb48e88447492f59
Emily Waters <121983559+emily-waters92@users.noreply.github.com>
2023-03-24 11:52:17 +1300

    Articles added

--- client/components/Articles.tsx
+++ client/components/Articles.tsx
@@ -1,5 +1,28 @@
 function Articles() {
-  return <h1>Articles placeholder</h1>
+  return (
+    <>
+      <div className="row">
+        <div className="column">
+          <img
+            src="/images/photo2.avif"
+            alt="Man with gloves typing on keyboard"
+          />
+          <p>Text about the photo</p>
+        </div>
+        <div className="column">
+          <img
+            src="/images/photo1.avif"
+            alt="Man coughing and holding hand up"
+          />
+          <p>Text about the photo</p>
+        </div>
+        <div className="column">
+          <img src="/images/photo3.avif" alt="Man holding up bunch of hair" />
+          <p>Text about the photo</p>
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default Articles
