5e8904e3933c819509971b62221e98af39b20880
sam-middleton <16142988+SamMiddleton@users.noreply.github.com>
2023-03-24 10:44:28 +1300

    WIP creating form

--- client/components/Form.tsx
+++ client/components/Form.tsx
@@ -1,5 +1,21 @@
-function Form() {
-  return <></>
+export interface FormData {
+  primary: string
+  secondary: string
+  tertiary: string
+}
+
+interface Props {
+  onSubmit: (data: FormData) => void
+}
+
+function Form({ onSubmit }: Props) {
+  return (
+    <form onSubmit={}>
+      <label htmlFor="primary">Primary Symptom: </label>
+      <label htmlFor="secondary">Secondary Symptom: </label>
+      <label htmlFor="tertiary">Tertiary Symptom: </label>
+    </form>
+  )
 }
 
 export default Form
