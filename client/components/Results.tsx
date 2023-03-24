864c881b77bdac6a21e49fafa94fd36554b6323c
humaidiar <humaidiar123@gmail.com>
2023-03-24 13:10:40 +1300

    update app.ts

--- client/components/Results.tsx
+++ client/components/Results.tsx
@@ -1,31 +1,28 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { getWordDone } from '../apiClient'
 
 interface AffirmationResponse {
   affirmation: string
 }
 
-function Results() {
+export default function Results() {
   const [textState, setText] = useState<AffirmationResponse | undefined>()
 
-  const clickHandler = () => {
-    return getWordDone()
+  useEffect(() => {
+    getWordDone()
       .then((response) => {
         setText(response)
       })
       .catch((err) => {
         console.log('Err messages: ' + err)
       })
-  }
+  }, [])
 
   return (
     <>
       <div>
-        <button onClick={clickHandler}>Get Feedback</button>
         <h2>{textState?.affirmation}</h2>
       </div>
     </>
   )
 }
-
-export default Results
