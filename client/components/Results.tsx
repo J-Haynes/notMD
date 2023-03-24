eda3a184fb1c5a6311de58765f6d6b84428e7229
J-Haynes <114152650+J-Haynes@users.noreply.github.com>
2023-03-24 14:23:18 +1300

    results page final

--- client/components/Results.tsx
+++ client/components/Results.tsx
@@ -7,12 +7,19 @@ interface Props {
 export default function Results({ disease, affirmation, onReset }: Props) {
   return (
     <>
-      <div>
-        <h2>You have {disease}.</h2>
-        <p>{affirmation}</p>
-        <button className="button-18" onClick={onReset}>
-          Get a new diagnosis
-        </button>
+      <div className="results-div">
+        <h2 className="disease">
+          Our qualified<span className="red">*</span> doctors have analysed your
+          symptoms and your diagnosis is:{' '}
+        </h2>
+        <p className="disease">{disease}</p>
+        <h2 className="affirmation">Our recommended course of action is: </h2>
+        <p className="affirmation">{affirmation}</p>
+        <div className="result-button-div">
+          <button className="button-18" onClick={onReset}>
+            Get a new diagnosis
+          </button>
+        </div>
       </div>
     </>
   )
